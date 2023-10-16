cb65fdaba9cfa856bcc764410244728f43f6aa80
selvakumarj1807 <selvakumarj1807@gmail.com>
2023-10-17 00:36:04 +0530

    host

--- settings.py
+++ settings.py
@@ -117,10 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-MEDIA_URL='/images/'
-MEDIA_ROOT=BASE_DIR/'static'
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS=[
     BASE_DIR/'static'
